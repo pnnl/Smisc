accfcb4a85cf0f23c74b596e0e12b6be493f2e06
Sego, Landon H <landon.sego@pnnl.gov>
2014-10-08 23:16:42 -0700

    Version 0.2.1, Inclusion of plotFun()

--- R/plotFun.R
+++ R/plotFun.R
@@ -7,47 +7,61 @@
 ##' @param xlim A numeric vector with two elements that define the domain over which the function(s) will be evaluated
 ##' and plotted, just as in \link{\code{plot.default}} in \pkg{graphics}.
 ##'
-##' @param col A vector of colors to use in the plotting. It's length should match the length of \code{fun}.
+##' @param col A vector of colors to use in the plotting. It's length should match the length of \code{fun}. See \code{\link{par}} for
+##' more info about the code{col} graphics parameter.
 ##'
-##' @param lty A vector of line types to use in the plotting. It's length should match the length of \code{fun}.
+##' @param lty A vector of line types to use in the plotting. It's length should match the length of \code{fun}. See \code{\link{par}} for
+##' more info about the code{lty} graphics parameter.
+##'
+##' @param type A single character indicating the type of plotting. This is passed to the \code{type} argument of
+##' \code{\link{plot.default}}.
 ##'
 ##' @param legendLabels A text vector with descriptive labels of each function that will be provided in the legend.
 ##' If \code{NULL}, no legend is drawn. This text vector is passed to the \code{legend} argument in \code{\link{legend}}
 ##' from the \pkg{graphics} package.
 ##'
-##' @param relX A numeric value in [0, 1] designating the relative horizontal (x) position of the legend on the plot.
+##' @param relX A numeric value in [0, 1] designating the relative horizontal (x) position of the legend in the plot.
 ##'
-##' @param relY A numeric value in [0, 1] designating the relative vertical (y) position of the legend on the plot.
+##' @param relY A numeric value in [0, 1] designating the relative vertical (y) position of the legend in the plot.
 ##'
 ##' @param nPoints The number of points that are evaluated and plotted for each function in \code{xlim}.
 ##'
 ##' @param \dots Additional graphical arguments passed to \code{\link{plot.default}}, \code{\link{lines}}, and
-##' \code{\link{legend}}.
+##' \code{\link{legend}}.  If an argument name specified in \code{\dots} matches an argument name in any
+##' of these three functions, the argument is passed to that function.
+##' For example, the line width, \code{lwd}, would be passed to all three (\code{\link{plot.default}}, \code{\link{lines}}, and
+##' \code{\link{legend}}).
+##' 
 ##'
 ##' @return The plot of the function(s)
 ##'
 ##' @author Landon Sego
 ##'
 ##' @examples
-# A single function
-f <- function(x) x^2
-plotFun(f, c(-2, 3), col = "Black", lty = 2)
+##'# A single function with a single argument
+##'f <- function(x) x^2
+##'plotFun(f, c(-2, 3), col = "Black", lty = 2, las = 1)
+##'
+##'# A handful of beta density functions, note how they take a single argument
+##'fList <- list(function(x) dbeta(x, 10, 10),
+##'              function(y) dbeta(y, 3, 3),
+##'              function(z) dbeta(z, 0.5, 0.50))
+##'
+##'# Plot them all on the same plot
+##'plotFun(fList, c(0.0001, 0.9999), ylim = c(0, 3.5),
+##'        col = c("Red", "Black", "Blue"), lty = rep(1, 3),
+##'        xlab = "x", ylab = expression(f(x)),
+##'        legendLabels = c("a = 10, b = 10", "a = 3, b = 3", "a = 0.5, b = 0.5"),
+##'        relX = 0.6, relY = 1, lwd = 3, main = "Gamma Densities")
+##'
 
-# A handful of beta density functions, note how they take a single argument
-fList <- list(function(x) dbeta(x, 10, 10),
-              function(y) dbeta(y, 3, 3),
-              function(z) dbeta(z, 0.5, 0.50))
-
-# Plot them all on the same plot
-plotFun(fList, c(1e-10, 1 - 1e-10),
-        col = c("Red", "Black", "Blue"), lty = rep(1, 3),
-        xlab = "x", ylab = expression(f(x)),
-        legendLabels = c("a = 10, b = 10", "a = 3, b = 3", "a = 0.5, b = 0.5"),
-        relX = 0.8, relY = 0.5)
-
-
-plotFun <- function(fun, xlim, col = rainbow(length(fun)), lty = 1:length(fun),
-                    legendLabels = NULL, relX = 0.7, relY = 0.9, 
+plotFun <- function(fun, xlim,
+                    col = rainbow(length(fun)),
+                    lty = 1:length(fun),
+                    type = "l",
+                    legendLabels = NULL,
+                    relX = 0.7,
+                    relY = 0.9, 
                     nPoints = 1000, ...) {
     
   # If fun is a single function, make it a list
@@ -81,11 +95,20 @@ plotFun <- function(fun, xlim, col = rainbow(length(fun)), lty = 1:length(fun),
   # Calculate the y values for each function
   yvals <- lapply(fun, function(x) do.call(x, list(xvec)))
 
-  # Determine the ylim
-  ylimVals <- range(unlist(lapply(yvals, range)))
-
   # Create the list with args for the plot.default command
   graphArgs <- list(...)
+
+  # Add in ylims if they're not present
+  if (!("ylim" %in% names(graphArgs))) 
+    graphArgs$ylim <- range(unlist(lapply(yvals, range)))
+
+  # For prettier default axis labels
+  x <- xvec
+  y <- yvals[[1]]
+  
+  # Default optional args
+  plotArgs1 <- list(x = quote(x), y = quote(y), type = type, xlim = xlim,
+                    lty = lty[1], col = col[1])
 
   # Select arguments from graphArgs that could be used in plot.default()
   plotArgs2 <- ifelse1(as.logical(length(graphArgs)),
@@ -93,15 +116,7 @@ plotFun <- function(fun, xlim, col = rainbow(length(fun)), lty = 1:length(fun),
                                  setdiff(unique(c(names(formals(plot.default)),
                                                   names(par()))), "...")],
                        list())
-
-  # Default optional args
-  plotArgs1 <- list(x = xvec, y = yvals[[1]], type = "l", xlim = xlim,
-                    ylim = ifelse1("ylim" %in% names(plotArgs2),
-                                   plotArgs2$ylim, ylimVals),
-                    lty = lty[1], col = col[1])
-
-  browser()
-  
+    
   # Make the first plot
   do.call(plot, c(plotArgs1, plotArgs2))
 
@@ -113,7 +128,7 @@ plotFun <- function(fun, xlim, col = rainbow(length(fun)), lty = 1:length(fun),
                         graphArgs[names(graphArgs) %in% names(par())],
                         list())
 
-    # Plot the addiional functions
+    # Plot the additional functions
     for (i in 2:length(fun))
       do.call(lines, c(list(x = xvec, y = yvals[[i]], lty = lty[i], col = col[i]), lineArgs))
   }
@@ -131,7 +146,7 @@ plotFun <- function(fun, xlim, col = rainbow(length(fun)), lty = 1:length(fun),
 
     # Make the legend
     do.call(legend, c(list(x = xlim[1] + relX * diff(xlim),
-                           y = ylimVals[1] + relY * diff(ylimVals),
+                           y = graphArgs$ylim[1] + relY * diff(graphArgs$ylim),
                            legend = legendLabels,
                            lty = lty,
                            col = col),
